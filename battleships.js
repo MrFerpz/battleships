ad44efed6a257901f077264f8ea7f7555262d1a7
Fred <leferpz@gmail.com>
2024-08-03 14:20:48 -0400

    Added basic gameboard and accompanying tests

--- battleships.js
+++ battleships.js
@@ -3,20 +3,59 @@ class Ship {
         this.length = length;
         this.hitNo = hitNo;
         this.isSunk = isSunk;
+        this.coordinates = [];
     }
 
+    // function to see if ship is still alive
     checkIsSunk() {
-        if (length === hitNo) {
-            isSunk = true;
+        if (this.length === this.hitNo) {
+            this.isSunk = true;
         }
     }
 
+    // function to declare a hit on the ship
     hit() {
         this.hitNo++;
         this.checkIsSunk();
     }
 }
 
-let ship1 = new Ship(5);
+class gameBoard {
+    constructor() {
+        // set-up empty board when it's new
+        this.board = 
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    }
 
-module.exports = Ship;
+    // placing ship requires co-ordinates, length, and direction
+    // y is first because that's how the array sees the rows
+    placeShip(y, x, length, direction = "horizontal") {
+
+        let ship = new Ship(length);
+
+        if (direction === "horizontal") {
+            for (let i = 0; i < length; i++) {
+                this.board[y][x + i] = 1;
+                ship.coordinates.push([y][x + i]);
+            }
+        }
+
+        else if (direction === "vertical") {
+            for (let i = 0; i < length; i++) {
+                this.board[y + i][x] = 1;
+                ship.coordinates.push([y + i][x]);
+            }
+        }
+    }
+}
+
+module.exports = {Ship, gameBoard};
--- battleships.test.js
+++ battleships.test.js
@@ -1,8 +1,20 @@
-const Ship = require('./battleships')
+const { Ship, gameBoard } = require('./battleships')
 
 test('Class has properties', () => {
     let ship1 = new Ship(5);
-    expect((ship1.length)).toBeTruthy();
-    expect((ship1.isSunk)).toBeFalsy();
-    expect((ship1.hitNo)).toBe(0);
+    expect(ship1.length).toBeTruthy();
+    expect(ship1.isSunk).toBeFalsy();
+    expect(ship1.hitNo).toBe(0);
+})
+
+test('Gameboard is set-up correctly', () => {
+    let gameBoard1 = new gameBoard();
+    expect(gameBoard1.board.length).toBe(10);
+})
+
+test('Place ship works', () => {
+    let gameBoard2 = new gameBoard();
+    gameBoard2.placeShip(3, 4, 5);
+    expect(gameBoard2.board[3][4]).toBe(1);
+    expect(gameBoard2.board[3][6]).toBe(1);
 })
