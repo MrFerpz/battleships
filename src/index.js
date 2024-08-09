fb774b4c3073dba9217a820332aedd0b49878263
Fred <leferpz@gmail.com>
2024-08-09 16:29:49 -0400

    Added naming and organisation to the cells, which now hover blue

--- src/index.js
+++ src/index.js
@@ -11,9 +11,14 @@ let playerOne = new Player();
 let CPU = new Player("computer");
 
 for (let i = 0; i < playerOne.playerBoard.board.length; i++) {
+    let row = document.createElement("div");
+    row.classList.add(`player-row`);
+    row.setAttribute("id", `player-row${i}`);
+    DOM.$playerBoard.appendChild(row);
     for (let j = 0; j < playerOne.playerBoard.board[0].length; j++) {
         let cell = document.createElement("div");
-        cell.classList.add("playerCell");
-        DOM.$playerBoard.appendChild(cell);
+        cell.classList.add("player-cell");
+        cell.setAttribute("id", `player-cell${i}-${j}`);
+        row.appendChild(cell);
     }
 }
